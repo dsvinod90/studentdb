2094c8b13924af6764ad028e78c46740c7195acb
Mrinmayee Rane <mrinmayeerane2810@gmail.com>
2023-11-15 22:03:24 -0500

    functionality for remove student with flash

--- route.js
+++ route.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { createOrUpdateStudent, readAllStudents, readStudent, deleteStudent } from './database/index.js';
 import { render } from 'pug';
 
+
 const router = express.Router();
 router.use(express.urlencoded({
     extended: true
@@ -17,6 +18,7 @@ const errorHandler = (err, req, res, next) => {
 const validateParams = (req, res, next) => {
   console.log(req.query)
   const { name, number } = req.query;
+  console.log(req.query)
   if (!name || !number) {
     return res.status(400).json({ success: false, message: "Invalid request parameters" });
   }
@@ -81,14 +83,23 @@ router.post('/student', async (req, res) => {
 /* 
 * Delete a student
 */
-router.delete('/student', validateParams, async (req, res) => {
+router.get('/remove-student', validateParams, async (req, res) => {
   const name = req.query.name;
   const number = req.query.number;
   const { success, data } = await deleteStudent(name, number);
   if (success) {
-    return res.json({ success, data });
+    req.flash("success", "Removed Student Successfully!");
+    res.render('index', {success: req.flash('success')});
+  } else {
+    // res.status(404).json({ success: false, message: "Student not found" });
+    req.flash('failure', 'Failed to remove student. Please try again.');
+    res.render('index', {failure: req.flash('failure')});
   }
-  res.status(404).json({ success: false, message: "Student not found" });
+});
+
+
+router.get('/remove', async (req, res) => {
+  res.render('remove')
 });
 
 // Use the error handling middleware
